9b41910d3e8f6fb51be6dfa3d364ee8fe45d45ec
EldieSoft <150984579+EldieSoft@users.noreply.github.com>
2024-02-13 20:16:01 -0500

    Update Calc.c
    A complete overhaul of the original code
    -Functions are now grouped together (add/sub, mul/div)
    -New exponent mode instead of square and cube functions
    -Functions now take parameters instead of just returning floats

--- Calc.c
+++ Calc.c
@@ -1,102 +1,131 @@
-/*Basic Calculator program for me to get back into C
- * Will clean up later, for now just a pet project
+/*Basic Command Line Calculator built in C
  *
- *Pick a number between 1 and 5, and the magical computer box will do the coresponding operation.
+ *Pick a number between 1 and 3, and the magical computer box will do the coresponding operation.
  *
- *1 = add, 2 = sub, 3 = mul, 4 = div, 5 = square, 6 = cube
+ *1 = add/sub, 2 = mul/div, 3 = exponent
  *
- * Big Bosco 2/9/24
+ * Big Bosco 2/13/24
  *
- * Version 1.0.2
+ * Version 1.2
  * 
 */ 
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int get_operation()//function to get operation
+int get_operation_type()//function to get operation
 {
 	int operation;
-	printf("What is your operation?\n1: ADD\t 2: SUB\t 3: MUL\t 4: DIV\t 5: SQR\t 6: CUB\t 0: CLOSE\n");
+	printf("What is your operation?\n1: ADD/SUB\t 2: MULT/DIV\t 3: EXP\t 0: CLOSE\n");
 	scanf("%d", &operation);
 	return operation;
 }
-float add()//function to add two numbers
+
+//mathematical functions
+float add(float num1, float num2)//function to add two numbers
 {
-	float num1, num2;
-	printf("What do you want to add?\n");
-	scanf("%f%f", &num1, &num2);
-	printf("Adding: %.2f and %.2f\n", num1, num2);
+	printf("%.2f + %.2f = ", num1, num2);
 	return num1 + num2;	
 }
-float sub()//function to subtract one number from another
+float sub(float num1, float num2)//function to subtract one number from another
 {
-	float num1, num2;
-	printf("What do you want to subtract?\n");
-	scanf("%f%f", &num1, &num2);
-	printf("Subtracting: %.2f and %.2f\n", num1, num2);
+	printf("%.2f - %.2f = ", num1, num2);
 	return num1 - num2;
 }
-float divide()//doesn't follow the theme, but 'div' is already a function name in stdlib.h
+float divide(float num1, float num2)//doesn't follow the theme, but 'div' is already a function name in stdlib.h
 {//also, function to divide 2 numbers
-	float num1, num2;
-	printf("What do you want to divide?\n");
-	scanf("%f%f", &num1, &num2);
-	printf("Dividing: %.2f and %.2f\n", num1, num2);
+	printf("%.2f / %.2f = ", num1, num2);
 	return num1 / num2;
 }
-float mul()//function to multiply 2 numbers
+float mul(float num1, float num2)//function to multiply 2 numbers
 {
-	float num1, num2;
-	printf("What do you want to multiply?\n");
-	scanf("%f%f", &num1, &num2);
-	printf("Multiplying: %.2f and %.2f\n", num1, num2);
+	printf("%.2f * %.2f = ", num1, num2);
 	return num1 * num2;
 }
-float sqr()//function to square number
+float expon(float num, int times)//function to exponent?
 {
-	float num;
-	printf("What do you want to square?\n");
-	scanf("%f",&num);
-	printf("Squaring: %.2f\n",num);
-	return num * num;
+	float e = 1.0; int i;
+	printf("%.2f to the power of %d = ", num, times);
+	for (i = 0; i < times; i++)
+	{
+		e *= num;
+	}
+	return e;
 }
-float cub()//function to cube number
+
+//finding operation functions, (as in add vs sub, mult vs divide etc.)
+float add_or_sub()
 {
-	float num;
-	printf("What do you want to cube?\n");
-	scanf("%f",&num);//missing ampersand broke the entire program last time
-	printf("Cubing: %.2f\n",num);
-	return num * num * num;
+	float num1, num2, end_prod; int choice;
+	printf("Pick two numbers:\n");
+	scanf("%f%f",&num1,&num2);
+	printf("ADD or SUB? (1 for ADD, 2 for SUB)\n");
+	scanf("%d",&choice);
+	switch (choice)
+	{
+		case 1:
+			end_prod = add(num1, num2);
+			break;
+		case 2:
+			end_prod = sub(num1, num2);
+			break;
+		default:
+			printf("ERROR\n");
+			end_prod = 0;
+			break;
+	}
+	return end_prod;
 }
- 
+float mul_or_div()
+{
+	float num1, num2, end_prod; int choice;
+	printf("Pick two numbers\n");
+	scanf("%f%f", &num1, &num2);
+	printf("MUL or DIV? (1 for MUL, 2 for DIV)\n");
+	scanf("%d", &choice);
+	switch (choice)
+	{
+		case 1:
+			end_prod = mul(num1, num2);
+			break;
+		case 2:
+			end_prod = divide(num1, num2);
+			break;
+		default:
+			printf("ERROR\n");
+			end_prod = 0;
+			break;
+	}
+	return end_prod;
+}
+float exponent_mayhem()
+{
+	float num, end_prod; int times;
+	printf("Pick a number\n");
+	scanf("%f", &num);
+	printf("%.2f to the power of what?\n", num);
+	scanf("%d", &times);
+	end_prod = expon(num, times);
+}
+
 int main()
 {
 	system("clear");
 	int opp; float numberino;
-	opp = (int)get_operation();
+	opp = (int)get_operation_type();
 	printf("%d\n" ,opp);
 	while (opp != 0)
 	{
 		switch (opp)
 		{
 			case 1:
-				numberino = add();
+				numberino = add_or_sub();
 				break;
 			case 2:
-				numberino = sub();
+				numberino = mul_or_div();
 				break;
 			case 3:
-				numberino = mul();
-				break;
-			case 4:
-				numberino = divide();
-				break;
-			case 5:
-				numberino = sqr();
-				break;
-			case 6:
-				numberino = cub();
+				numberino = exponent_mayhem();
 				break;
 			default:
 				printf("ERROR\n");
@@ -104,7 +133,7 @@ int main()
 				break;
 		}
 		printf("%.2f\n", numberino);
-		opp = (int)get_operation();
+		opp = (int)get_operation_type();
 	}
 	printf("Goodbye\n");
 	return 0;
